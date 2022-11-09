28d0a613adacb91f83e2ce7864d999e38abb462c
mdmohsin2 <mohsinbd862554@gmail.com>
2022-11-09 12:00:57 +0600

    client site to setup mongodb

--- index.js
+++ index.js
@@ -13,20 +13,39 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kjb9ctc.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('assignment11').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(error => {
+    console.log(error);
+})
 
 
 
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('assignment 11 server is running')
 })
 
