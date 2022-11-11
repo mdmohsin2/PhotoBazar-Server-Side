cd53dc7f2a3a3f7e91fdeb82f9c82c906d0d2f49
mdmohsin2 <mohsinbd862554@gmail.com>
2022-11-11 19:23:18 +0600

    completed backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,15 +18,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// // jwt function
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'unauthorized access' })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
+
+
+
+
 async function run() {
     try {
         const serviceCollection = client.db('assignment11').collection('services');
         const reviewCollection = client.db('assignment11').collection('review');
 
+        // // jwt
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body;
+        //     console.log(user);
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+        //     res.send({ token })
+        // })
+
+
+
         // services api
         app.get('/services', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query).limit(3).sort({_id: -1});
+            const cursor = serviceCollection.find(query).limit(3).sort({ _id: -1 });
             const services = await cursor.toArray();
             res.send(services)
         });
@@ -33,7 +63,7 @@ async function run() {
         // all services api
         app.get('/serviceAll', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({ _id: -1 });
             const services = await cursor.toArray();
             res.send(services)
         });
@@ -70,7 +100,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
 
         app.get('/review', async (req, res) => {
             let query = {};
@@ -93,7 +123,7 @@ async function run() {
                     review: req.query.review
                 }
             }
-            const cursor = reviewCollection.find(query).sort({_id: -1});
+            const cursor = reviewCollection.find(query).sort({ _id: -1 });
             const users = await cursor.toArray();
             res.send(users);
         });
@@ -104,24 +134,24 @@ async function run() {
 
 
         // review button edit
-        app.get('/singleReview/:id', async(req,res)=>{
+        app.get('/singleReview/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const user = await reviewCollection.findOne(query)
             res.send(user)
         })
 
-        app.put('/singleReview/:id', async(req,res)=>{
+        app.put('/singleReview/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: ObjectId(id)}
+            const filter = { _id: ObjectId(id) }
             const user = req.body;
-            const option = {upsert: true};
+            const option = { upsert: true };
             const updatedUser = {
                 $set: {
                     message: user.message,
                 }
             }
-            const result= await reviewCollection.updateOne(filter, updatedUser, option);
+            const result = await reviewCollection.updateOne(filter, updatedUser, option);
             res.send(result)
         })
 
